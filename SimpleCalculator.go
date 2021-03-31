7c00601920ca2e61270faf381d5209621db22e01
berthot <matheus.berthot@gmail.com>
2021-03-31 15:49:30 -0300

    simple calculator

---
+++ SimpleCalculator.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	c := Calculator{2, 2}
+	print(c.addition())
+}
+
+type Calculator struct {
+	v1, v2 int
+}
+
+func (c *Calculator) division() int {
+	return c.v1 / c.v2
+}
+
+func (c *Calculator) multiplication() int {
+	return c.v1 * c.v2
+}
+
+func (c *Calculator) addition() int {
+	return c.v1 + c.v2
+}
+
+func (c *Calculator) subtraction() int {
+	return c.v1 - c.v2
+}
